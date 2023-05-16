fa3b7843d92e3ad074520e116b7326bd6474ce89
2Venus <102713414+Venus2Mice@users.noreply.github.com>
2023-05-16 14:25:40 +0700

    final

--- login.php
+++ login.php
@@ -14,18 +14,33 @@ if(isset($_POST['submit'])){
 
    $email = $_POST['email'];
    $email = filter_var($email, FILTER_SANITIZE_STRING);
-   $pass = sha1($_POST['pass']);
+   $pass = $_POST['pass'];
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
 
    $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
    $select_user->execute([$email, $pass]);
    $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
-   if($select_user->rowCount() > 0){
-      $_SESSION['user_id'] = $row['id'];
-      header('location:home.php');
+   if(empty($email) || empty($pass)){
+      if(empty($email) && empty($pass)){
+         $message[] = 'Vui lòng điền thông tin đăng nhập';
+      }
+
+      if(empty($email) && !empty($pass)){
+         $message[] = 'vui lòng điền email!';
+      }
+
+      if(!empty($email) && empty($pass)){
+         $message[] = 'vui lòng điền password!';
+      }
+      
    }else{
-      $message[] = 'incorrect username or password!';
+      if($select_user->rowCount() > 0){
+         $_SESSION['user_id'] = $row['id'];
+         header('location:home.php');
+      }else{
+         $message[] = 'email hoặc mật khẩu không chính xác!';
+      }
    }
 
 }
@@ -59,8 +74,10 @@ if(isset($_POST['submit'])){
 
    <form action="" method="post">
       <h3>login now</h3>
-      <input type="email" name="email" required placeholder="enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="pass" required placeholder="enter your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="email" name="email" placeholder="enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')"
+      value="<?php echo (!empty($email)) ? $email:false;?>">
+      <input type="password" name="pass" placeholder="enter your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')"
+      value="<?php echo (!empty($pass)) ? $pass:false;?>">
       <input type="submit" value="login now" name="submit" class="btn">
       <p>don't have an account? <a href="register.php">register now</a></p>
    </form>
--- register.php
+++ register.php
@@ -10,6 +10,13 @@ if(isset($_SESSION['user_id'])){
    $user_id = '';
 };
 
+    $errorPassword="";
+    $errorRepass="";
+    $errorFullname="";
+    $errorPhone="";
+    $errorEmail="";
+
+
 if(isset($_POST['submit'])){
 
    $name = $_POST['name'];
@@ -18,21 +25,104 @@ if(isset($_POST['submit'])){
    $email = filter_var($email, FILTER_SANITIZE_STRING);
    $number = $_POST['number'];
    $number = filter_var($number, FILTER_SANITIZE_STRING);
-   $pass = sha1($_POST['pass']);
+   $pass = $_POST['pass'];
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
-   $cpass = sha1($_POST['cpass']);
+   $cpass = $_POST['cpass'];
    $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
 
-   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? OR number = ?");
-   $select_user->execute([$email, $number]);
+   $check1 = 0; $check2 = 0; $check3 = 0; $check4 = 0; $check5 = 0; $check6 = 0;
+
+
+
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+   $select_user->execute([$email]);
    $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
-   if($select_user->rowCount() > 0){
-      $message[] = 'email or number already exists!';
+
+   if(empty($name)){
+      $errorFullname = "Vui lòng nhập họ và tên";
+      $check3++;
+  }
+  else{
+      if(!preg_match("/^[a-zA-Z ]*$/",$name)){
+          $errorFullname = 'Họ và tên sai định dạng';
+          $check3++;
+      }
+      else{
+          $errorFullname="";
+          $check3=0;
+      }
+  }
+
+  if(empty($email)){
+   $errorEmail = "Vui lòng nhập email";
+   $check4++;
+}
+else{
+   if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+       $errorEmail = 'Email sai định dạng';
+       $check4++;
    }else{
-      if($pass != $cpass){
-         $message[] = 'confirm password not matched!';
-      }else{
+   
+   if($select_user->rowCount() > 0){
+      $errorEmail = 'email đã tồn tại';
+      $check4++;
+   }
+   else{
+      $errorEmail='';
+      $check4=0;
+   }
+}
+}
+
+if(empty($number)){
+   $errorPhone = "Vui lòng nhập số điện thoại";
+   $check5++;
+}
+else{
+   if(!preg_match('/^[0-9]{10}+$/', $number)){
+       $errorPhone = 'Số điện thoại sai định dạng';
+       $check5++;
+   }
+   else{
+       $errorPhone="";
+       $check5=0;
+   }
+}
+
+
+  if(empty($pass)){
+      $errorPassword = "Vui lòng nhập mật khẩu";
+      $check1++;
+  }
+  else{
+      if(strlen(trim($pass)) < 8 || strlen(trim($pass)) > 16){
+          $errorPassword = 'Mật khẩu phải có 8-16 ký tự';
+          $check1++;
+      }
+      else{
+          $errorPassword="";
+          $check1=0;
+      }
+  }
+
+  if(empty($cpass)){
+      $errorRepass = "Vui lòng nhập lại mật khẩu";
+      $check2++;
+  }
+  else{
+      if(strcmp($pass,$cpass) != 0 ){
+          $errorRepass = 'Nhập lại mật khẩu không khớp';
+          $check2++;
+      }
+      else{
+          $errorRepass="";
+          $check2=0;
+      }
+  }
+
+   $validate = $check1 + $check2 + $check3 + $check4 + $check5 ;
+         if($validate==0){
          $insert_user = $conn->prepare("INSERT INTO `users`(name, email, number, password) VALUES(?,?,?,?)");
          $insert_user->execute([$name, $email, $number, $cpass]);
          $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
@@ -41,7 +131,6 @@ if(isset($_POST['submit'])){
          if($select_user->rowCount() > 0){
             $_SESSION['user_id'] = $row['id'];
             header('location:home.php');
-         }
       }
    }
 
@@ -61,7 +150,8 @@ if(isset($_POST['submit'])){
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <!-- <link rel="stylesheet" href="css/style.css"> -->
+   <link rel="stylesheet" href="css/style.css?php echo time(); ?>">
    <!-- icons -->
    <link rel="shortcut icon" href="images/favicon.png" type="">
    <title>register</title>
@@ -76,11 +166,22 @@ if(isset($_POST['submit'])){
 
    <form action="" method="post">
       <h3>register now</h3>
-      <input type="text" name="name" required placeholder="enter your name" class="box" maxlength="50">
-      <input type="email" name="email" required placeholder="enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="number" name="number" required placeholder="enter your number" class="box" min="0" max="9999999999" maxlength="10">
-      <input type="password" name="pass" required placeholder="enter your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="cpass" required placeholder="confirm your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="text" name="name" placeholder="enter your name" class="box" maxlength="50"
+      value="<?php echo (!empty($name)) ? $name:false;?>">
+      <span class="error"><?php echo $errorFullname;?></span>
+      <input type="text" name="email" placeholder="enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')"
+      value="<?php echo (!empty($email)) ? $email:false;?>">
+      <span class="error"><?php echo $errorEmail;?></span>
+      <input type="text" name="number" placeholder="enter your number" class="box" min="0" max="9999999999" maxlength="10"
+      value="<?php echo (!empty($number)) ? $number:false;?>">
+      <span class="error"><?php echo $errorPhone;?></span>
+      <input type="password" name="pass" placeholder="enter your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')"
+      value="<?php echo (!empty($pass)) ? $pass:false;?>">
+      <span class="error"><?php echo $errorPassword;?></span>
+      <input type="password" name="cpass" placeholder="confirm your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')"
+      value="<?php echo (!empty($cpass)) ? $cpass:false;?>">
+      <span class="error"><?php echo $errorRepass;?></span>
+      <br><br><br>
       <input type="submit" value="register now" name="submit" class="btn">
       <p>already have an account? <a href="login.php">login now</a></p>
    </form>
